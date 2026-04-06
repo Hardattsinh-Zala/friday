ac252df1199377cad0078bf9f8dd14503d6a4751
Hardattsinh Zala <zalahardatt@gmail.com>
2026-04-06 13:14:55 +0530

    Removed unnecessary build

--- friday.sh
+++ friday.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -e # Exit early if any commands fail
 
 # Load environment variables from .env
@@ -5,10 +6,4 @@ if [ -f "$(dirname "$0")/.env" ]; then
     export $(grep -v '^#' "$(dirname "$0")/.env" | xargs)
 fi
 
-(
-  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
-)
-
-exec $(dirname "$0")/build/Debug/claude-code.exe "$@"
+exec $(dirname "$0")/build/Debug/friday.exe "$@"
